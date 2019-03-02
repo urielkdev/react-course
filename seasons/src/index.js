f6805c39ab990f8fb050fb42b22fd580b86e2702
Uriel Kindermann <urielkindermann@gmail.com>
2019-03-01 22:03:51 -0300

    Spinner added, for waiting geolocation request

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
     state = { lat: null, errorMessage: '' }
@@ -26,9 +27,7 @@ class App extends React.Component {
             
         }
         return (
-            <div>
-                loading
-            </div>
+                <Spinner message='Waiting for your permition'></Spinner>
         );
     }
 }
