1b26cdb650d8c34e6ee744d610009d859b248189
Uriel Kindermann <urielkindermann@gmail.com>
2019-03-03 00:31:06 -0300

    some updates, React.createRef() added to manage the rows of grid

--- pictures/src/components/ImageCard.js
+++ pictures/src/components/ImageCard.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
 
 class ImageCard extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { spanHeight: 10, spans: 0 };
+
+        this.imageRef = React.createRef();
+    }
+
+    componentDidMount() {
+        this.imageRef.current.addEventListener('load', this.setSpans /* callback */);
+    }
+
+    // calculates and sets how many spans are needed for the image to fit
+    setSpans = () => {
+
+        const height = this.imageRef.current.clientHeight;
+
+        const spans = Math.ceil(height / this.state.spanHeight);
+        this.setState({ spans });
+    }
+
     render() {
         const { description, urls } = this.props.image;
 
         return (
-            <div>
-                <img alt={description}
-                    src={urls.regular}
-                />
+            // n ta multiplicando os gridColunEnd
+            <div style={{gridAutoRows: `${this.state.spanHeight}px`,
+                gridRowEnd: `span ${this.state.spans}`}}>
+                <img ref={this.imageRef} alt={description} src={urls.regular} />
             </div>
         );
     }
