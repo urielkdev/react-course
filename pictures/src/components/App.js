eea564574cf25aad51e189aebb1cbb82af96b212
Uriel Kindermann <urielkindermann@gmail.com>
2019-03-02 22:06:32 -0300

    async with arrow function to bind the function

--- pictures/src/components/App.js
+++ pictures/src/components/App.js
@@ -5,7 +5,7 @@ import SearchBar from './SearchBar';
 class App extends Component {
     state = { images: [] };
 
-    async onSearchBarSubmit(term) {
+    onSearchBarSubmit = async (term) => {
         const response = await axios.get('https://api.unsplash.com/search/photos', {
             params: {
                 query: term
@@ -14,16 +14,17 @@ class App extends Component {
                     Authorization: 'Client-ID 3b0605464256a33b9c3b041e5ee90345c429f1d08ad684cc87636409b09dec42'
                 }
         });
-        console.log(response.data.results[0].urls);
+        this.setState({images: response.data.results});
     }
 
     render() {
         return (
             <div className="ui container" style={{marginTop: '10px'}}>
                 <SearchBar onSubmit={this.onSearchBarSubmit}
-                // is not mandatory the name of this callback to be onSubmit, 
-                // I used this because it makes sense in this case
+                    // is not mandatory the name of this callback to be onSubmit, 
+                    // I used this because it makes sense in this case
                 />
+                {this.state.images.length} images
             </div>
         );
     }
