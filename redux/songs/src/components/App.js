8b1d6eed40183ddb778cd82ed8a0b4a91c76c52a
Uriel Kindermann <urielkindermann@gmail.com>
2019-03-08 20:27:31 -0300

    README updated

--- redux/songs/src/components/App.js
+++ redux/songs/src/components/App.js
@@ -1,13 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-    render() {
-        return (
-            <div>
-                aa       
-            </div>
-        );
-    }
-}
+const App = () => {
+    return (
+        <div>
+            asd    
+        </div>
+    );
+};
 
 export default App;
