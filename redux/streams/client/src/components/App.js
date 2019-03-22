aecd12b8a5cc7fc41740fe2489744f59c0116439
Uriel Kindermann <urielkindermann@gmail.com>
2019-03-21 23:06:09 -0300

    basic react-router use

--- redux/streams/client/src/components/App.js
+++ redux/streams/client/src/components/App.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+const PageOne = () => {
+    return (
+        <div>
+            PageOne
+            <Link to="/page-two">Go to page 2</Link>
+        </div>
+    );
+}
+
+const PageTwo = () => {
+    return (
+        <div>
+            PageTwo
+            <Link to="/">Go to page 1</Link>
+        </div>
+    );
+}
 
 class App extends Component {
     render() {
         return (
             <div>
-                asd
+                <BrowserRouter>
+                    <div>
+                        <Route path="/" exact component={PageOne} />
+                        <Route path="/page-two" component={PageTwo} />
+                    </div>
+                </BrowserRouter>
             </div>
         );
     }
