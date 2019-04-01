1c0375e3fbca650e8fc42deb2f100e6d876a63ec
Uriel Kindermann <urielkindermann@gmail.com>
2019-04-01 00:20:36 -0300

    implemented StreamEdit using StreamForm

--- redux/streams/client/src/components/streams/StreamEdit.js
+++ redux/streams/client/src/components/streams/StreamEdit.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchStream } from '../../actions'
+import _ from 'lodash';
+import { fetchStream, editStream } from '../../actions'
+import StreamForm from './StreamForm';
 
 class StreamEdit extends Component {
     componentDidMount() {
         this.props.fetchStream(this.props.match.params.id);
+    }
+
+    onSubmit = (formValues) => {
+        this.props.editStream(this.props.match.params.id, formValues);
     }
 
     render() {
@@ -13,7 +19,12 @@ class StreamEdit extends Component {
         }
         return (
             <div>
-                {this.props.stream.title}       
+                <h3>Edit Stream</h3>
+                <StreamForm
+                    initialValues={_.pick(this.props.stream, 'title', 'description')}
+                    // initialValues={{title: this.props.stream.title, description: this.props.stream.description}}
+                    onSubmit={this.onSubmit}
+                />
             </div>
         );
     }
@@ -25,4 +36,4 @@ const mapStateToProps = (state, ownProps) => {
     };
 };
 
-export default connect(mapStateToProps, {fetchStream})(StreamEdit);
+export default connect(mapStateToProps, {fetchStream, editStream})(StreamEdit);
