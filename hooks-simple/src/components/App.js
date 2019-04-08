f805be5a7358cf36689edd046de1abca4de099b2
Uriel Kindermann <urielkindermann@gmail.com>
2019-04-07 22:32:53 -0300

    code reuse with Hooks

--- hooks-simple/src/components/App.js
+++ hooks-simple/src/components/App.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import ResourceList from './ResourceList';
+import UserList from './UserList';
 
 const App = () => {
     const [resource, setResource] = useState('posts');
 
     return (
         <div className="ui container">
+            <UserList />
             <div>
                 <button onClick={() => setResource('posts')} className="ui primary button">
                     Posts
