744f401eb45956d155834c2cd06f6ddb7a348f9a
urielkdev <urielkindermann@gmail.com>
2019-04-07 23:42:32 -0300

    refactored code using Hooks

--- seasons-hooks/src/index.js
+++ seasons-hooks/src/index.js
@@ -2,42 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import Spinner from './Spinner';
+import useLocation from './useLocation';
 
-class App extends React.Component {
-    state = { lat: null, errorMessage: '' }
-
-    componentDidMount() {
-        window.navigator.geolocation.getCurrentPosition(
-            (data) => this.setState({lat: data.coords.latitude}),
-            (err) => this.setState({errorMessage: err.message})
-        );
+const App = () => {
+    const {lat, errorMessage} = useLocation();
+    
+    let content;
+    if (errorMessage) {
+        content = <div>Error: {errorMessage}</div>;
+    } else if (lat) {
+        content = <SeasonDisplay lat={lat}></SeasonDisplay>;
+    } else {
+        content = <Spinner message='Waiting for your permition'></Spinner>;
     }
 
-    // to exemplify that the return can be in a function and
-    // can be put inside a div for example (when called)
-    renderContent() {
-        if (this.state.errorMessage && !this.state.lat) {
-            return (
-                <div>
-                    Error: {this.state.errorMessage}
-                </div>
-            );
-        } else if (!this.state.errorMessage && this.state.lat) {
-            return (
-                <SeasonDisplay lat={this.state.lat}></SeasonDisplay>
-            );
-            
-        }
-        return (
-                <Spinner message='Waiting for your permition'></Spinner>
-        );
-    }
-
-    render() {
-        return (
-            this.renderContent()
-        );
-    }
-}
+    return <div className="border red">{content}    </div>;
+    
+};
 
 ReactDOM.render(<App />, document.querySelector('#root'));
